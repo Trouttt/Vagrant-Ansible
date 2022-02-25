f50510f7785b9c62dec19f1b15e27780b3a9db1f
Leonardo da Silva Siqueira <desu987@hotmail.com>
2022-02-25 20:06:27 -0300

    Added(MV): added db01 folder

---
+++ db01/provision.sh
@@ -0,0 +1,4 @@
+#/bin/sh
+cat << EOT >> /home/vagrant/.ssh/authorized_keys
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC8ire99d1fbiOON5ekPEjLlATL6eoi/D0FbojeHxv0+CuXmCyL/KaL6y3/McnEUMsYhKrmT2yURvv5TzhhfQ3Eq2u9GqXwC8lG1QTnj2wZUV0IaFj1rPQ/Nq/qEYYgJAnzkQSZyxcyjsag5mFQoEvPX1zVwGAEDDIJqnHzerUW1eoPJK7vmh6iRDN9SrWtS2lzUorQKgLJjX7qG7j6e++nLnG7h4Sd5LuL1TLAnZ/IYkH4c+wszRdhS9GEMY8CVI8VJRpZFxFZNWdPq78pnKzglBZJ2me4fcbsF4HWxZzQkp1I51I/pBLaZki8/Ji44F1H2LPylXzizhZHBGwAmiLB vagrant@control-node
+EOT
